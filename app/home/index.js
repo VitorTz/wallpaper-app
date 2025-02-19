ccd597d7a1198e5a4c7cbba2838e0fde65aee446
VitorTz <vitor.fsz@proton.me>
2025-02-18 23:56:33 -0300

    Update

--- app/home/index.js
+++ app/home/index.js
@@ -1,34 +1,63 @@
 import { Pressable, StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { theme } from '../../constants/themes'
 import { wp, hp } from '../../helpers/common'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-        <View style={styles.container}>
-            <StatusBar backgroundColor='white' style='light' ></StatusBar>
-            
-            <View style={styles.header}>
-                <Text style={styles.title}>Pixels</Text>
-                <Pressable hitSlop={{top: 10, bottom: 10, left: 20, right: 10}}>
-                    <View style={styles.menuButton}>
-                        <View style={styles.line} ></View>
-                        <View style={styles.line} ></View>
-                        <View style={styles.line} ></View>
+
+    const [text, setText] = useState('');
+
+    const searchImages = async() => {
+        console.log("oi")
+    }
+
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <View style={styles.container}>
+                <StatusBar backgroundColor='white' style='light' ></StatusBar>
+                
+                <View style={styles.header}>
+                    <Text style={styles.title}>Pixels</Text>
+                    <Pressable hitSlop={{top: 10, bottom: 10, left: 20, right: 10}}>
+                        <View style={styles.menuButton}>
+                            <View style={styles.line} ></View>
+                            <View style={[styles.line, {marginLeft: 4}]} ></View>
+                            <View style={styles.line} ></View>
+                        </View>
+                    </Pressable>
+                </View>
+
+                <View style={{flexDirection: 'row', alignItems: "center", justifyContent: "space-between", columnGap: 10}} >
+                    <View style={{flex: 1}} >
+                        <TextInput 
+                            defaultValue={text}
+                            onChangeText={newText => setText(newText)}  
+                            style={styles.textInput} 
+                            placeholder='search' >
+                        </TextInput>
+                        <Pressable 
+                            style={styles.closeIcon}
+                            onPress={() => setText('')}
+                            hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}
+                        >
+                            <Ionicons name="close-circle-outline" size={24} color="black" />
+                        </Pressable>
                     </View>
-                </Pressable>
+
+                    <View style={{height: hp(6), width: 48, backgroundColor: "black", alignItems: "center", justifyContent: "center"}} >
+                        <Pressable onPress={searchImages}>
+                            <Ionicons name="send" size={24} color="white" />
+                        </Pressable>
+                    </View>
+                                        
+                </View>
+
             </View>
-
-            <View style={{width: '100%'}} >
-                <TextInput style={styles.textInput} ></TextInput>
-
-            </View>
-
-        </View>
-    </SafeAreaView>
+        </SafeAreaView>
   )
 }
 
@@ -37,29 +66,28 @@ export default HomeScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+        paddingHorizontal: wp(4),
+        paddingVertical: hp(3),
         backgroundColor: theme.colors.white
     },
     title: {
-        fontWeight: theme.fontWeights.medium,
+        fontWeight: theme.fontWeights.semiBold,
         fontSize: hp(4)
     },
     menuButton: {
-        padding: 8, 
-        backgroundColor: theme.colors.black, 
+        padding: 8,         
         rowGap: 5        
     },
     line: {
         width: wp(5), 
-        height: 2, 
-        backgroundColor: theme.colors.white
+        height: 3, 
+        backgroundColor: theme.colors.black
     },
     textInput: {
-        width: '100%',
         height: hp(6),
-        paddingVertical: 20,
         paddingHorizontal: 10,
-        borderWidth: 1,
+        color: theme.colors.black,
+        borderWidth: 2,
         borderColor: theme.colors.black
     },
     header: {
@@ -68,5 +96,14 @@ const styles = StyleSheet.create({
         alignItems: "center", 
         justifyContent: "space-between", 
         marginBottom: 16
+    },
+    closeIcon: {                
+        position: 'absolute',                
+        right: 0,
+        top: 0,
+        bottom: 0,
+        marginRight: 10,
+        justifyContent: "center",
+        alignItems: "center",
     }
 })
